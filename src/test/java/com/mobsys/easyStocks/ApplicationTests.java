36d24874ff03890bb4b411f3704924ff4419d198
Till Pilarczyk <till.pilarczyk@study.hs-duesseldorf.de>
2021-11-06 18:12:01 +0100

    fix tests

--- src/test/java/com/mobsys/easyStocks/ApplicationTests.java
+++ src/test/java/com/mobsys/easyStocks/ApplicationTests.java
@@ -2,8 +2,10 @@ package com.mobsys.easyStocks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ApplicationTests {
 
 	@Test

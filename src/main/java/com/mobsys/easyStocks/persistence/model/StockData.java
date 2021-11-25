a3e67b28425b51b9874aa3daf33f2bc5c74425be
sabroona <sabrina.cielas@gmail.com>
2021-11-25 11:16:28 +0100

    wip

--- src/main/java/com/mobsys/easyStocks/persistence/model/StockData.java
+++ src/main/java/com/mobsys/easyStocks/persistence/model/StockData.java
@@ -1,4 +1,54 @@
 package com.mobsys.easyStocks.persistence.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="stocksData")
 public class StockData {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    private String symbol;
+
+    @Column
+    private Float adjustedClose;
+
+    @Column
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(final String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Float getAdjustedClose() {
+        return adjustedClose;
+    }
+
+    public void setAdjustedClose(final Float adjustedClose) {
+        this.adjustedClose = adjustedClose;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(final Date date) {
+        this.date = date;
+    }
 }

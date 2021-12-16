e7399f87392efbedbe711a6d7706e6e63d86ba45
proohit <direnc99@gmail.com>
2021-12-16 15:38:45 +0100

    feat: allow api docs

--- src/main/java/com/mobsys/easyStocks/config/WebSecurityConfig.java
+++ src/main/java/com/mobsys/easyStocks/config/WebSecurityConfig.java
@@ -56,12 +56,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/login", "/register", "/stocks/**").permitAll().
+                .authorizeRequests().antMatchers("/login", "/register", "/stocks/**", "/swagger-resources/**",
+                        "/swagger-ui/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/webjars/**")
+                .permitAll()
                 // all other requests need to be authenticated
-                anyRequest().authenticated().and().
+                .anyRequest().authenticated().and()
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
-                exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Add a filter to validate the tokens with every request

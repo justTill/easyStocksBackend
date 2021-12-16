e36c516f6d66560d348e9d46c896407f345dc4a5
proohit <direnc99@gmail.com>
2021-12-16 15:39:39 +0100

    feat: add token docs

---
+++ src/main/java/com/mobsys/easyStocks/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.mobsys.easyStocks.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sig-Predict REST API Document")
+                .description("work in progress")
+                .termsOfServiceUrl("localhost")
+                .version("1.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("jwtToken", "Authorization", "header");
+    }
+}
--- src/main/java/com/mobsys/easyStocks/controller/WatchlistController.java
+++ src/main/java/com/mobsys/easyStocks/controller/WatchlistController.java
@@ -9,20 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+
 @RestController
 public class WatchlistController {
 
     @GetMapping(path = "/watchlist")
+    @ApiOperation(value = "", authorizations = { @Authorization(value = "jwtToken") })
     public GetWatchlistResponseDto getWatchlist() {
         return null;
     }
 
     @PostMapping(path = "/watchlist/{symbol}")
+    @ApiOperation(value = "", authorizations = { @Authorization(value = "jwtToken") })
     public PostWatchlistResponseDto addToWatchlist(@PathVariable String symbol) {
         return null;
     }
 
     @DeleteMapping(path = "/watchlist/{symbol}")
+    @ApiOperation(value = "", authorizations = { @Authorization(value = "jwtToken") })
     public String deleteFromWatchlist(@PathVariable String symbol) {
         return "OK";
     }

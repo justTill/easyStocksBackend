79322b44ded2c7707f2e63b37d2beb5f3f939ddc
proohit <direnc99@gmail.com>
2021-12-23 12:00:11 +0100

    feat: add user context watchlist fns

--- src/main/java/com/mobsys/easyStocks/controller/WatchlistController.java
+++ src/main/java/com/mobsys/easyStocks/controller/WatchlistController.java
@@ -1,41 +1,48 @@
 package com.mobsys.easyStocks.controller;
 
-import com.mobsys.easyStocks.models.dtos.GetWatchlistResponseDto;
-import com.mobsys.easyStocks.models.dtos.PostWatchlistResponseDto;
-
-import com.mobsys.easyStocks.persistence.model.StockLatestData;
-import com.mobsys.easyStocks.persistence.repository.StockRepository;
-import com.mobsys.easyStocks.persistence.repository.WatchlistRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
+
+import com.mobsys.easyStocks.persistence.model.StockLatestData;
+import com.mobsys.easyStocks.persistence.repository.UserRepository;
+import com.mobsys.easyStocks.persistence.repository.WatchlistRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
 
-@RestController
+@RestController()
 public class WatchlistController {
 
     @Autowired
     private WatchlistRepository watchlistRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping(path = "/watchlist")
     @ApiOperation(value = "", authorizations = { @Authorization(value = "jwtToken") })
     public List<StockLatestData> getWatchlist() {
-        UUID watchlist_id = UUID.fromString("0a0399e6-9a71-4bab-976a-5ac0e2ac516d");
-
-        return watchlistRepository.findStockDataLatestWatchlist(watchlist_id);
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UUID watchlistId = userRepository.findFirstByMail(user.getUsername()).getWatchlistId();
+        return watchlistRepository.findStockDataLatestWatchlist(watchlistId);
     }
 
-    @RequestMapping (path = "/watchlist/{symbol}")
+    @PostMapping(path = "/watchlist/{symbol}")
     @ApiOperation(value = "", authorizations = { @Authorization(value = "jwtToken") })
-    public String  addToWatchlist(@PathVariable String symbol) {
-        UUID watchlist_id = UUID.fromString("0a0399e6-9a71-4bab-976a-5ac0e2ac516d");
-        watchlistRepository.addToWatchlist(symbol, watchlist_id);
-
-        return "created";
+    public List<StockLatestData> addToWatchlist(@PathVariable String symbol) {
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UUID watchlistId = userRepository.findFirstByMail(user.getUsername()).getWatchlistId();
+        watchlistRepository.addToWatchlist(symbol, watchlistId);
+        return watchlistRepository.findStockDataLatestWatchlist(watchlistId);
     }
 
     @DeleteMapping(path = "/watchlist/{symbol}")

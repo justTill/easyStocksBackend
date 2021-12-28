a1006995ddf50d7975eb0bfed7595a2c2c236c5b
Till Pilarczyk <till.pilarczyk@study.hs-duesseldorf.de>
2021-12-28 11:18:16 +0100

    cleanup

--- src/main/java/com/mobsys/easyStocks/service/StockUpdateService.java
+++ src/main/java/com/mobsys/easyStocks/service/StockUpdateService.java
@@ -86,13 +86,6 @@ public class StockUpdateService {
         }
     }
 
-    //TODO: Remove after testing
-    @EventListener(ApplicationReadyEvent.class)
-    public void updateNotifications() {
-        saveNotifications();
-    }
-
-
     @Scheduled(cron = "0 30 23 * * *", zone = "EST")
     public final void updateDailyStockData() {
         logger.info("Start getting Daily Stock Data");
